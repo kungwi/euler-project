e1bd767cfc8a3a9cb140bfa6898f6d0ddcceddc6
kungwi <c_mouratidis@icloud.com>
2016-10-02 22:34:20 +0300

    Merge origin/master

--- src/Problems001_050/problem002.java
+++ src/Problems001_050/problem002.java
@@ -1,26 +1,25 @@
 package Problems001_050;
-
-
 /**
  *
  * @author thales
  */
 public class problem002 {
 
-    int limit ; //  = 4000000 ;
+    private int limit ; //  = 4000000 ;
     
     public problem002(int a) {
         limit = a ;
     }
     
     public int solve() {
+       
         int pr = 8 , prpr = 2 , ans = 10 ;
         
             while(4*pr+prpr<=limit) {
                 int temp=prpr ; 
                 prpr = pr ; 
                 pr = 4*pr+temp ;
-                ans += pr ;  }
+                ans += pr ;  }  
     
     return ans ; }
 }
