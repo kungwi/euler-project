79a883a808dd11e210590030aca0b98e5f94d51f
kungwi <c_mouratidis@icloud.com>
2016-10-11 23:44:01 +0300

    Merge origin/master

--- src/mainRun.java
+++ src/mainRun.java
@@ -5,10 +5,8 @@ import java.io.IOException;
  * @author thales
  */
 public class mainRun {
-    
         public static void main(String[] args) throws IOException {
         long start_time = System.currentTimeMillis();
         problem034 pr = new problem034() ;
         System.out.println(pr.solve()); 
-        System.out.println("running time: " + (System.currentTimeMillis() - start_time)); }
-}
+        System.out.println("running time: " + (System.currentTimeMillis() - start_time)); } }
